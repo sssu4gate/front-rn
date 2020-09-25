ac434716f1d85c57a30984c62b23868d7009a733
구현모 <koo@Koo.local>
2020-09-25 20:19:55 +0900

    navigation

---
+++ Tabs/Community.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text , Button} from 'react-native';
+
+export default function Community(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Community!</Text>
+        </View>
+      );
+}
---
+++ Tabs/Write.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text , Button} from 'react-native';
+
+export default function Write(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Write!</Text>
+        </View>
+      );
+}
