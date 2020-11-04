13f03eec6f4eb4a880dcb3ffe8d004ca6bffefa9
chltj <scsc0511@naver.com>
2020-11-04 15:42:03 +0900

    add tob navigation bar in community

--- screens/Community.js
+++ screens/Community.js
@@ -1,10 +1,52 @@
 import * as React from 'react';
-import { View, Text , Button} from 'react-native';
+import { View, Text , StyleSheet} from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+function Popularity(){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <Text>Popularity!</Text>
+    </View>
+  )
+}
+
+function Trend(){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <Text>Trend!</Text>
+    </View>
+  )
+}
+
+function Loco(){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <Text >Loco!</Text>
+    </View>
+  )
+}
+
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function Community(){
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Community!</Text>
-        </View>
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintiColor : 'white',
+            inactiveTintColor : 'white',
+            indicatorStyle : {backgroundColor : 'white'},
+            style: {backgroundColor : "#FF6DA0"}
+          }}>           
+            <Tab.Screen name="Popularity" component={Popularity}/>
+            <Tab.Screen name="Trend" component={Trend}/>
+            <Tab.Screen name="Loco" component={Loco}/>
+        </Tab.Navigator>
       );
 }
+
+const styles = StyleSheet.create({
+  tab_text :{
+    color : '#FFFFFF'
+  }
+});
