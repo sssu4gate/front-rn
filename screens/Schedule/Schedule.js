11a6f2df2bae05e55ef4ea15b3904a95df159554
seokh1213 <seokh1213@gmail.com>
2020-11-24 14:22:38 +0900

    feat: schedule 페이지 수정

--- screens/Schedule/Schedule.js
+++ screens/Schedule/Schedule.js
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import { View, Text , Button} from 'react-native';
+import { View, Text , Button, TouchableOpacity} from 'react-native';
 import {Calendar} from 'react-native-calendars';
+import { FlatList } from "react-native-gesture-handler";
 import * as theme from "../../assets/theme";
 import LeftArrow from "../../assets/images/LeftArrow";
 import RightArrow from "../../assets/images/RightArrow";
 
 export default function Schedule({navigation, route, course}){
-  const todyDate = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`;
-  const [date, setDate] = React.useState({todayDate:{selected:true}});
+  let todayDate = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`;
+  const selectDate=(date)=>{
+    let temp = {}
+    temp[date]={selected:true};
+    return temp;
+  }
+  const [date, setDate] = React.useState(selectDate(todayDate));
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', width:"100%", height:"100%" }}>
+    <View style={{ flex: 1, alignItems: 'center', width:"100%", height:"100%", backgroundColor:"#fff" }}>
       <View style={{width:"100%"}}>
         <Calendar
           markedDates={date}
@@ -18,12 +24,8 @@ export default function Schedule({navigation, route, course}){
             todayTextColor: theme.PRIMARY_COLOR,
             selectedDayBackgroundColor: theme.PRIMARY_COLOR
           }}
-          onDayPress={day =>{
-            console.log(day);
-            const temp = {}
-            temp[day.dateString] = {selected:true};
-            setDate(temp)
-          }}
+          onDayPress={date =>setDate(selectDate(date.dateString))
+          }
           monthFormat={"yyyy년 MMM"}
           renderArrow={(direction) => (
             <View style={{width:20, height:20}}>
@@ -31,8 +33,28 @@ export default function Schedule({navigation, route, course}){
             </View>
           )}
         />
-        <Text>123</Text>
       </View>
+      <View style={{width:"100%", height:1, backgroundColor:"#e5e5e5"}}/>
+      <Text style={{width:110, fontSize:20, marginTop:10, color:theme.SECOND_TEXT_COLOR}}>{Object.keys(date)}</Text>
+      <FlatList 
+        style={{width:"100%", flex:1, padding:15}}
+        data={[1,2,3]}
+        renderItem={
+          ({item, index})=>{
+            return (
+              <TouchableOpacity 
+                key={index} 
+                style={{height:22, flexDirection:"row", marginBottom:15}}
+                onPress={()=>{
+                  console.log(item, index);
+                }}
+              >
+                <Text style={{fontSize:18, color:theme.SECOND_TEXT_COLOR}}>{item}</Text>
+              </TouchableOpacity>
+            );
+          }
+        }
+      />
     </View>
   );
 }
