d1290f936e87eb833d3ed6500517f43b3e996893
@hyunmo.urssu <koo@Koo.local>
2020-11-26 20:36:38 +0900

    settings

--- screens/MyProfile/SetProfile.js
+++ screens/MyProfile/SetProfile.js
@@ -6,6 +6,7 @@ import {
   Image,
   TouchableOpacity,
   StyleSheet,
+  TextInput,
 } from "react-native";
 import Modal from "react-native-modal";
 
@@ -14,6 +15,8 @@ export default function SetProfile({ navigation }) {
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
+  const [queryString, setQueryString] = useState("");
+  const [modal, setModal] = useState("");
 
   return (
     <>
@@ -29,27 +32,133 @@ export default function SetProfile({ navigation }) {
           <Text style={style.area2_text}>asd</Text>
         </View>
         <View style={style.area3}>
-          <TouchableOpacity onPress={toggleModal}>
+          <TouchableOpacity
+            onPress={() => {
+              toggleModal();
+              setModal("name");
+            }}
+          >
             <Text style={style.area3_text}>닉네임 변경</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={style.area3_text}>비밀번호 변경</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              toggleModal();
+              setModal("area");
+            }}
+          >
             <Text style={style.area3_text}>관심지역 변경</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={style.area3_text}>성별</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={style.area3_text}>생일</Text>
-          </TouchableOpacity>
           <Modal isVisible={isModalVisible}>
-            <View>
-              <Text>Hello!</Text>
-              <Button title="Hide modal" onPress={toggleModal} />
-            </View>
+            {modal === "name" && (
+              <ChangeName
+                name={queryString}
+                setQueryString={setQueryString}
+                toggleModal={toggleModal}
+              />
+            )}
+            {modal === "area" && <ChangeArea toggleModal={toggleModal} />}
           </Modal>
+        </View>
+      </View>
+    </>
+  );
+}
+
+function ChangeName({ name, setQueryString, toggleModal }) {
+  return (
+    <>
+      <View style={style.modal}>
+        <Text style={style.text}>변경할 닉네임</Text>
+        <TextInput
+          onChangeText={setQueryString}
+          style={{
+            color: "#000000",
+            width: "50%",
+            height: "10%",
+            fontSize: 20,
+            weight: "bold",
+            alignSelf: "center",
+            borderWidth: 1,
+            borderColor: "#777777",
+            fontSize: 14,
+            borderRadius: 10,
+            padding: 5,
+          }}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            alignSelf: "center",
+            padding: 10,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              toggleModal();
+              console.log(name);
+            }}
+            style={style.modalBtn}
+          >
+            <Text>변경</Text>
+          </TouchableOpacity>
+          <Text> </Text>
+          <TouchableOpacity
+            onPress={() => {
+              toggleModal();
+              console.log(name);
+            }}
+            style={style.modalBtn}
+          >
+            <Text>취소</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+}
+
+function ChangeArea({ toggleModal }) {
+  const dummy = [
+    "관악구",
+    "동작구",
+    "서초구",
+    "강남구",
+    "마포구",
+    "강서구",
+    "강동구",
+  ];
+  return (
+    <>
+      <View style={style.modal}>
+        <Text style={style.text}>지역을 선택하세요</Text>
+        <View>
+          {dummy.map((area) => (
+            <TouchableOpacity
+              onPress={() => {
+                console.log(area);
+                toggleModal();
+              }}
+            >
+              <Text>{area}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignSelf: "center",
+            padding: 10,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              toggleModal();
+              console.log(name);
+            }}
+            style={style.modalBtn}
+          >
+            <Text>취소</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </>
@@ -93,5 +202,24 @@ const style = StyleSheet.create({
     height: 100,
     borderRadius: 50,
     alignSelf: "center",
+  },
+  modal: {
+    alignSelf: "center",
+    width: 250,
+    height: 200,
+    backgroundColor: "#e2e2e2",
+    borderRadius: 10,
+    alignContent: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  modalBtn: {
+    padding: 5,
+    borderWidth: 1,
+    borderColor: "#777777",
+    borderRadius: 10,
+  },
+  text: {
+    padding: 5,
   },
 });
